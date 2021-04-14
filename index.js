5b56e880b529fb01eaa6088962ee9e75e76457ab
xmlnsbuzz <xmlnsbuzz@gmail.com>
2021-04-14 17:30:08 +0900

    Chapter04-JS-DataTypeps-and-Variables

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-console.log( "Hello from external JavaScript file" )
+// /**
+//  * EXAMPLE 1 - Variable Declaration.
+//  */
+
+// var a;
+
+// console.log( a );
+// // console.log( b );
+// // Uncaught ReferenceError: b is not defined
+
+// let c;
+
+// console.log( c );
+
+// // const d;
+// // Uncaught SyntaxError: Missing initializer in const declaration;
+
+// /**
+//  * EXAMPLE 2 - "let"
+//  */
+
+// let myNumber = 10;
+
+// console.log( myNumber );
+
+// // declaration
+// let myString;
+
+// // assignment
+// myString = "hello from string";
+
+// console.log( myString );
+
+// // reassignment
+// myString = "New value for my String."
+
+// console.log(myString)
+
+
+/**
+ * EXAMPLE 3 - "var"
+ */
+
+// 
+
+
+// 
+
+/**
+ * CHALLENGE 2
+ */
+
+// // let myObject = {
+// //   x: 10,
+// //   y: true
+// // };
+
+// let x = 10;
+// const y = true;
+// const myObject = {
+//   a: x,
+//   b: y
+// };
+
+// console.log( myObject );
+
+// x = 20;
+// let anotherObject;
+// anotherObject = {
+//   newA: x,
+//   b: y,
+//   c: myObject
+// };
+
+// console.log( anotherObject );
+
+/**
+// NOTE JS를 dynamic type language라 부르는 이유는 
+ * x=10으로 할당한 변수 x에 x=true로 data type을 바꾸어도 상관없기(에러가 안난다.) 때문이다.
+ */
