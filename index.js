e3b6264d6abd90368f518bf6083ddf730136a211
xmlnsbuzz <xmlnsbuzz@gmail.com>
2021-04-14 18:31:00 +0900

    JS Variable and Data Types End

--- index.js
+++ index.js
@@ -78,3 +78,33 @@
 // NOTE JS를 dynamic type language라 부르는 이유는 
  * x=10으로 할당한 변수 x에 x=true로 data type을 바꾸어도 상관없기(에러가 안난다.) 때문이다.
  */
+// let myVariable;
+// console.log( myVariable );
+
+// myVariable = 10;
+// console.log( myVariable );
+
+// myVariable = true;
+// console.log( myVariable );
+
+// myVariable = {
+//   x: true,
+//   y: 10
+// }
+// console.log(myVariable)
+
+let myVar;
+myVar = null;
+console.log( myVar );
+
+myVar = 15;
+console.log( myVar );
+
+myVar = false;
+console.log( myVar );
+
+
+
+
+
+
